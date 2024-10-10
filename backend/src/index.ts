62c3f080257a208d5d9c8ab8cddef8bb77a137cc
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-10-10 10:08:05 -0300

    feat: add express initial server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,14 @@
-const message: string = "Hello, TypeScript!";
-console.log(message);
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});

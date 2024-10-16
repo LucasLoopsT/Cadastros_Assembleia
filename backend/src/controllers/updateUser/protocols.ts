86d031261dd5196f7032ac65ed88d963a2cea9b1
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-10-16 16:46:18 -0300

    feat: add MongoUpdateUserRepository

---
+++ backend/src/controllers/updateUser/protocols.ts
@@ -0,0 +1,27 @@
+import { User, Congregacao, Cargo } from "../../models/user";
+import { HttpResponse, HttpRequest } from "../protocols";
+
+export interface IUpdateUserController {
+  handle(
+    HttpRequest: HttpRequest<UpdateUserParams>
+  ): Promise<HttpResponse<User>>;
+}
+
+export interface UpdateUserParams {
+  nome?: string;
+  sobrenome?: string;
+  foto?: string;
+  cpf?: string;
+  rg?: string;
+  dataNasc?: string;
+  cidade?: string;
+  bairro?: string;
+  rua?: string;
+  numEndereco?: number;
+  congregacao?: Congregacao[];
+  cargo?: Cargo[];
+}
+
+export interface IUpdateUserRepository {
+  updateUser(id: string, params: UpdateUserParams): Promise<User>;
+}

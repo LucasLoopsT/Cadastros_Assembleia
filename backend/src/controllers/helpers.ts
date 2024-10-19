9898487688d48dc4017ce91c8e388d604e0185a0
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-10-19 14:59:26 -0300

    feat: enum httpStatusCode

--- backend/src/controllers/helpers.ts
+++ backend/src/controllers/helpers.ts
@@ -1,25 +1,25 @@
-import { HttpResponse } from "./protocols";
+import { HttpResponse, HttpStatusCode } from "./protocols";
 
 export const ok = <T>(body: any): HttpResponse<T> => ({
-  statusCode: 200,
+  statusCode: HttpStatusCode.OK,
   body: body,
 });
 
 export const created = <T>(body: any): HttpResponse<T> => ({
-  statusCode: 201,
+  statusCode: HttpStatusCode.CRETED,
   body: body,
 });
 
 export const badRequest = (message: string): HttpResponse<string> => {
   return {
-    statusCode: 400,
+    statusCode: HttpStatusCode.BAD_REQUEST,
     body: message,
   };
 };
 
 export const serverError = (error: unknown): HttpResponse<string> => {
   return {
-    statusCode: 500,
+    statusCode: HttpStatusCode.SERVER_ERROR,
     body: `Something went wrong: ${error}.`,
   };
 };
--- backend/src/controllers/protocols.ts
+++ backend/src/controllers/protocols.ts
@@ -1,5 +1,5 @@
 export interface HttpResponse<T> {
-  statusCode: number;
+  statusCode: HttpStatusCode;
   body: T;
 }
 
@@ -7,6 +7,13 @@ export interface HttpRequest<B> {
   body?: B;
   params?: any;
   headers?: any;
+}
+
+export enum HttpStatusCode {
+  OK = 200,
+  CRETED = 201,
+  BAD_REQUEST = 400,
+  SERVER_ERROR = 500,
 }
 
 export interface IController {

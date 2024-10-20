8ff4f8deef8f95a45a56232e194fd93fc7f7b759
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-10-20 01:32:31 -0300

    feat: add CreateAdmController

---
+++ backend/src/controllers/Adm/createAdm/createAdm.ts
@@ -0,0 +1,43 @@
+import { badRequest, created, serverError } from "../../helpers";
+import { CreateAdmParams, ICreateAdmRepository } from "./protocols";
+import { HttpRequest, HttpResponse, IController } from "../../protocols";
+import { Adm } from "../../../models/adm";
+
+export class CreateAdmController implements IController {
+  constructor(private readonly createAdmRepository: ICreateAdmRepository) {}
+  async handle(
+    httpRequest: HttpRequest<CreateAdmParams>
+  ): Promise<HttpResponse<Adm | string>> {
+    try {
+      const body = httpRequest?.body;
+
+      const requiredFields: (keyof CreateAdmParams)[] = [
+        "nome",
+        "email",
+        "password",
+      ];
+
+      for (const field of requiredFields) {
+        if (!body?.[field as keyof CreateAdmParams]?.length) {
+          return badRequest(`Field ${field} is required.`);
+        }
+      }
+
+      const someFieldIsNotAllowedToCreate =
+        body &&
+        Object.keys(body).some(
+          (key) => !requiredFields.includes(key as keyof CreateAdmParams)
+        );
+
+      if (someFieldIsNotAllowedToCreate) {
+        return badRequest("Some received field is not allowed.");
+      }
+
+      const adm = this.createAdmRepository.createAdm(body!);
+
+      return created(adm);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}

bbdbb449ce77ba3ddf3e2009ae37be5b2b872ff0
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-10-21 22:43:49 -0300

    feat: add UpdateAdmController and Routes

---
+++ backend/src/controllers/Adm/updateAdm/updateAdm.ts
@@ -0,0 +1,44 @@
+import { HttpRequest, HttpResponse, IController } from "../../protocols";
+import { IUpdateAdmRepository, UpdateAdmParams } from "./protocols";
+import { badRequest, ok, serverError } from "../../helpers";
+import { Adm } from "../../../models/adm";
+
+export class UpdateAdmController implements IController {
+  constructor(private readonly updateAdmRepository: IUpdateAdmRepository) {}
+  async handle(
+    httpRequest: HttpRequest<UpdateAdmParams>
+  ): Promise<HttpResponse<Adm | string>> {
+    try {
+      const id = httpRequest?.params;
+      const body = httpRequest?.body;
+
+      if (!id) {
+        return badRequest("Missing admin ID.");
+      }
+
+      if (!body) {
+        return badRequest("Missing fields.");
+      }
+
+      const allowrdFieldsToUpdate: (keyof UpdateAdmParams)[] = [
+        "nome",
+        "email",
+        "password",
+      ];
+
+      const someFieldIsNotAllowedToUpdate = Object.keys(body).some(
+        (key) => !allowrdFieldsToUpdate.includes(key as keyof UpdateAdmParams)
+      );
+
+      if (someFieldIsNotAllowedToUpdate) {
+        return badRequest("Some received field is not allowed.");
+      }
+
+      const admin = await this.updateAdmRepository.updateUser(id, body);
+
+      return ok(admin);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
--- backend/src/routes/adm.ts
+++ backend/src/routes/adm.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { CreateAdmController } from "../controllers/Adm/createAdm/createAdm";
+import { UpdateAdmController } from "../controllers/Adm/updateAdm/updateAdm";
 import { MongoCreateAdmRepository } from "../repositories/Adm/createAdm/mongo-CreateAdm";
+import { MongoUpdateAdmRepository } from "../repositories/Adm/updateAdm/mongo-UpdateAdm";
 
 const admRouter = Router();
 
@@ -11,6 +13,19 @@ admRouter.post("/", async (req, res) => {
 
   const { body, statusCode } = await createAdmController.handle({
     body: req.body,
+  });
+
+  res.status(statusCode).send(body);
+});
+
+admRouter.patch("/:id", async (req, res) => {
+  const mongoUpdateAdmRepository = new MongoUpdateAdmRepository();
+
+  const updateAdmController = new UpdateAdmController(mongoUpdateAdmRepository);
+
+  const { body, statusCode } = await updateAdmController.handle({
+    body: req.body,
+    params: req.params,
   });
 
   res.status(statusCode).send(body);

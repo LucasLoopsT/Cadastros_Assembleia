8b257794d2da6a65cad37433f2077de24b8a4a07
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-10-30 10:17:42 -0300

    feat: homepage model

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,8 +1,16 @@
+import { Container } from "./style";
+
 function Home() {
   return (
-    <>
+    <Container>
       <h1>Welcome</h1>
-    </>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        sunt pariatur accusantium ex quibusdam sapiente vero corporis quae error
+        incidunt repellendus et quia, necessitatibus odit illo veritatis, rem
+        voluptate! Rem.
+      </p>
+    </Container>
   );
 }
 

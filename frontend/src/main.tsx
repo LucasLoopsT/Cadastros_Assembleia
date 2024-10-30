c65a28c672e7206cd1072d5ed06a45cd5dbb0876
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2024-10-29 21:11:04 -0300

    feat: architecture folders and structure to new pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import App from "./App.tsx";
+import Home from "./pages/home/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: "",
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ],
+  { basename: "/" }
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

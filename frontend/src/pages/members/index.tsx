2e90d2e8cbbde81b2c93e2c52e039992b0875636
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2025-01-13 14:37:01 -0300

    feat: useState Searchbox and filters

--- frontend/src/pages/members/index.tsx
+++ frontend/src/pages/members/index.tsx
@@ -1,10 +1,20 @@
 import { Container, MembersArea, Search } from "./style.tsx";
+import { useState } from "react";
 import MemberCard from "../../components/MemberCard/index.tsx";
 import FillterBtn from "../../components/FiltterBtn/index.tsx";
 import { HiMagnifyingGlassCircle } from "react-icons/hi2";
 
 function Members() {
-  const handleFilter = (id: string) => {
+  const [members, setMembers] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSearchMembers = () => {
+    alert(message);
+    alert(filter);
+  };
+
+  const handleFilter = (id: any) => {
     const btnsActiveteds = document.getElementsByClassName("active");
     Array.from(btnsActiveteds).forEach((btn) => {
       btn.classList.remove("active");
@@ -12,13 +22,22 @@ function Members() {
 
     const btn = document.getElementById(id);
     btn?.classList.add("active");
+    setFilter(id);
   };
 
   return (
     <Container>
       <Search>
-        <HiMagnifyingGlassCircle id="lupa" />
-        <input type="text" placeholder="Digite aqui." />
+        <HiMagnifyingGlassCircle
+          id="lupa"
+          onClick={() => handleSearchMembers()}
+        />
+        <input
+          type="text"
+          placeholder="Digite aqui."
+          onChange={(e) => setMessage(e.target.value)}
+          id="searchBox"
+        />
         <p>Filtros:</p>
         <div className="filtros">
           <FillterBtn name={"Nome"} onClick={() => handleFilter("Nome")} />
